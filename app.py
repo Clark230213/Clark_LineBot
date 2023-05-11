12c820f726f7418e24c2df22cd02286d1a6ded7d
Clark <b@hflite.com.tw>
2023-05-11 15:50:59 +0800

    0511

--- app.py
+++ app.py
@@ -1,30 +1,62 @@
-# 載入需要的模組
-from __future__ import unicode_literals
-import os
-from flask import Flask, request, abort
+from flask import Flask, request
+
+# 載入 json 標準函式庫，處理回傳的資料格式
+import json
+import requests
+# 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-# LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('s47JHrUKfsC1fqVLWzp+OQ8Gj23Vr/rguDmLyeiCYrNaD/JLaxS3DlIAyevsikXaWhFdth0uoPEBTk9ZXPrEMzyj4l7WasiV9LTEY2x1flYRSc0jqkMohRa0gNKFgOBktyHPKjmpRepQTuOpDFlhcQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('c68430d99c0fedee7c7b3afe930010ab')
-
-# 接收 LINE 的資訊
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
+url = "https://api.openai.com/v1/chat/completions"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 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"
+}
+chat_history = []
+MAX_HISTORY_LENGTH = 20  # 設定 chat_history 最大長度
+@app.route("/", methods=['GET', 'POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        access_token = 's47JHrUKfsC1fqVLWzp+OQ8Gj23Vr/rguDmLyeiCYrNaD/JLaxS3DlIAyevsikXaWhFdth0uoPEBTk9ZXPrEMzyj4l7WasiV9LTEY2x1flYRSc0jqkMohRa0gNKFgOBktyHPKjmpRepQTuOpDFlhcQdB04t89/1O/w1cDnyilFU='
+        secret = 'c68430d99c0fedee7c7b3afe930010ab'
+        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
+        if type=='text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            print(msg)                                       # 印出內容
+            reply = msg
 
-    return 'OK'
+            chat_history.append({"role": "user", "content": msg})
+            data = {
+                "model": "gpt-3.5-turbo",
+                "messages": chat_history
+            }
+            response = requests.post(url, headers=headers, data=json.dumps(data))
+            response_json = response.json()
+            content = response_json['choices'][0]['message']['content']
+            print(content)
+            reply = content
+            # 判斷 chat_history 長度是否超過最大值，若是，刪除最早的一筆資料
+            if len(chat_history) >= MAX_HISTORY_LENGTH:
+                chat_history.pop(0)
+            chat_history.append({"role": "assistant", "content": content})
 
-if __name__ == "__main__":
+        else:
+            reply = '你傳的不是文字呦～'
+        print(reply)
+        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
+    except:
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+
+if __name__ == "__app__":
     app.run()
